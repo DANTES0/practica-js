a29ee1eb7d8189fbc47dfc8aff35e4ee6196c2a8
Hjjik <vaaa@mail.ru>
2023-07-03 21:01:20 +0300

    intrestingFactComplete

--- Scripts/intresting-fact.js
+++ Scripts/intresting-fact.js
@@ -19,3 +19,29 @@ document.getElementById('burger-line-wrapper').addEventListener('click', functio
       }
   
   })
+
+  function close(id_checkMenu, id_answer) {
+    const checker = document.getElementById(id_checkMenu)
+    const answer1 = document.getElementById(id_answer)
+    if(checker.checked) {
+        answer1.style.maxHeight = '400px';
+    } else 
+    {
+        answer1.style.maxHeight = '0';
+    }
+  }
+document.getElementById('question1').addEventListener('click', () => close('check-menu1', 'answer1'))
+document.getElementById('question2').addEventListener('click', () => close('check-menu2', 'answer2'))
+document.getElementById('question3').addEventListener('click', () => close('check-menu3', 'answer3'))
+document.getElementById('question4').addEventListener('click', () => close('check-menu4', 'answer4'))
+document.getElementById('question5').addEventListener('click', () => close('check-menu5', 'answer5'))
+document.getElementById('question6').addEventListener('click', () => close('check-menu6', 'answer6'))
+document.getElementById('question7').addEventListener('click', () => close('check-menu7', 'answer7'))
+document.getElementById('question8').addEventListener('click', () => close('check-menu8', 'answer8'))
+document.getElementById('question9').addEventListener('click', () => close('check-menu9', 'answer9'))
+document.getElementById('question10').addEventListener('click', () => close('check-menu10', 'answer10'))
+document.getElementById('question11').addEventListener('click', () => close('check-menu11', 'answer11'))
+document.getElementById('question12').addEventListener('click', () => close('check-menu12', 'answer12'))
+document.getElementById('question13').addEventListener('click', () => close('check-menu13', 'answer13'))
+document.getElementById('question14').addEventListener('click', () => close('check-menu14', 'answer14'))
+document.getElementById('question15').addEventListener('click', () => close('check-menu15', 'answer15'))
