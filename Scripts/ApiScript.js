e34e5d2ffdebb78315bc112faaa2869a565a916b
Hjjik <vaaa@mail.ru>
2023-07-03 20:18:11 +0300

    intrestingFact

--- Scripts/ApiScript.js
+++ Scripts/ApiScript.js
@@ -236,4 +236,24 @@ const fact = async() => {
 // news()
 
 
-
+// document.getElementById('burger-line-wrapper').addEventListener('click', function() {
+//     const checker = document.getElementById('check-menu')
+//     const burgerMenu = document.getElementById('burger-menu')
+//     console.log(burgerMenu)
+//     const name = document.getElementById('page-name')
+//     const japanFlag = document.getElementById('japan-flag')
+//     if (!checker.checked) {
+//         name.textContent = 'Японская атмосфера';
+//         japanFlag.style.display = 'block'  
+//         name.style.textShadow ='-3px 3px #558ABB'  
+//         checker.checked = true
+//         burgerMenu.style.maxHeight = '100vh'
+//     } else {
+//         checker.checked = false
+//         burgerMenu.style.maxHeight = '0'
+//         name.textContent = 'Изучение японских иероглифов';
+//         name.style.textShadow = 'none'
+//         japanFlag.style.display = 'none'
+//       }
+  
+//   })
