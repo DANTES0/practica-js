4ca189156211b42fdfca91864d879ef6733147b9
ritba meow <veselova.rita69@mail.ru>
2023-07-03 21:15:13 +0300

    g

--- Scripts/intresting-fact.js
+++ Scripts/intresting-fact.js
@@ -45,3 +45,56 @@ document.getElementById('question12').addEventListener('click', () => close('che
 document.getElementById('question13').addEventListener('click', () => close('check-menu13', 'answer13'))
 document.getElementById('question14').addEventListener('click', () => close('check-menu14', 'answer14'))
 document.getElementById('question15').addEventListener('click', () => close('check-menu15', 'answer15'))
+
+
+function revealText(text, selector, n_changes, duration_milliseconds, charset) {
+    let element = document.querySelector(selector)
+    let text_index = 0
+    let text_interval = setInterval(function() {
+      
+      let i = text_index
+      if (i >= text.length) {
+        clearInterval(text_interval)
+        return
+      }
+      
+      let span = document.createElement('span')
+      
+      if (text[i] == '\n' || text[i] == ' ') {
+        span.innerText = text[i]
+      } else {
+        span.innerText = charset[Math.floor(Math.random()*charset.length)]
+      }
+      span.style.opacity = '0.0'
+      element.appendChild(span)
+      span.style.transition = `all ${duration_milliseconds}ms ease-in`
+      
+      setTimeout(function() {
+        span.style.opacity = '1.0'
+        if (text[i] == '\n' || text[i] == ' ') return
+  
+        let count = 0
+        let interval = setInterval(function() {
+  
+          count += 1
+          if (count == n_changes) {
+            span.innerText = text[i]
+            clearInterval(interval)
+          } else {
+            span.innerText = charset[Math.floor(Math.random()*charset.length)]
+          }
+  
+        }, duration_milliseconds/n_changes)
+      }, 30)
+      text_index += 1
+    }, 1)
+    
+  }
+  
+  let alphabet = '一人七八九十入二三上下大女万土山千川子小くぞだなにねのぷゐゕ゜゜゜ィシ##**!!!&$$#^'
+  
+  let text = `  Япония привлекает путешественников с тех пор, как в 1853 году она впервые открылась для международной торговли. Несмотря на сохранение многочисленных традиций и культурных достопримечательностей, Япония также является одним из ведущих экономических и технологических центров мира. Древние боги и традиционные обычаи соседствуют с передовыми технологиями и модной поп-культурой, и всегда есть что-то новое, что можно испытать во время посещения.
+    На этой странице собраны самые интересные факты о Японии. Прочитав их, вы погрузитесь в японскую атмосферу и возможно захотите побывать в этой красивой и разнообразной стране.`
+  
+  revealText(text, '.text-fact', 5, 500, alphabet)
+  
